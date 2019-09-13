76240235f8b70089fd7b1bf961cd4d320826ee30
YounessFkhach <younessfkhach@protonmail.com>
2019-09-13 16:49:50 +0100

    add time loggin

--- lib/images/test.ex
+++ lib/images/test.ex
@@ -2,9 +2,13 @@ defmodule Images.Test do
   @timeout 60000
 
   def start do
+    time1 = DateTime.utc_now()
+
     1..1000
     |> Enum.map(fn i -> async_call_generate_qr_code("Some text here to encode", i) end)
     |> Enum.each(fn task -> await_and_inspect(task) end)
+
+    IO.puts Time.diff(DateTime.utc_now(), time1)
   end
 
   defp async_call_generate_qr_code(text, i) do
